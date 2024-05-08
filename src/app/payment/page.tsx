be88ef4107235c3634921336ed10c8650bec833a
it21294198 <it21294198@my.sllit.lk>
2024-05-08 23:22:41 +0530

    .

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -2,31 +2,6 @@
 import Script from 'next/script';
 import { useEffect } from 'react';
 
-interface PaymentObject {
-    sandbox: boolean;
-    merchant_id: string;
-    return_url?: string | undefined;
-    cancel_url?: string | undefined;
-    notify_url: string;
-    order_id: string;
-    items: string;
-    amount: string;
-    currency: string;
-    hash: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    address: string;
-    city: string;
-    country: string;
-    delivery_address: string;
-    delivery_city: string;
-    delivery_country: string;
-    custom_1?: string;
-    custom_2?: string;
-}
-
 export default function Payment() {
     useEffect(() => {
         const paymentScript = document.createElement('script');
--- src/app/payment/payhere.d.ts
+++ src/app/payment/payhere.d.ts
@@ -9,3 +9,28 @@ interface Payhere {
 interface Window {
   payhere: Payhere;
 }
+
+interface PaymentObject {
+  sandbox: boolean;
+  merchant_id: string;
+  return_url?: string | undefined;
+  cancel_url?: string | undefined;
+  notify_url: string;
+  order_id: string;
+  items: string;
+  amount: string;
+  currency: string;
+  hash: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  address: string;
+  city: string;
+  country: string;
+  delivery_address: string;
+  delivery_city: string;
+  delivery_country: string;
+  custom_1?: string;
+  custom_2?: string;
+}
