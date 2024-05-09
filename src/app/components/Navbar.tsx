ce0ad4e5a9f164166ee549cc6a0ebbac8d3c1e34
it21292422 <it21292422@my.sliit.lk>
2024-05-09 12:40:45 +0530

    feat: enroll page, my course, course

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
@@ -9,16 +10,16 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Explore Courses</a></li>
-                        <li><a>My Courses</a></li>
+                        <li><Link href="/learner/allcourse">Explore Courses</Link></li>
+                        <li><Link href="/learner/mycourse">My Courses</Link></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl text-main">/Courzinger</a>
+                <Link href="/learner" className="btn btn-ghost text-xl text-main">/Courzinger</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-lg">
-                    <li><a>Explore Courses</a></li>
-                    <li><a>My Courses</a></li>
+                    <li><Link href="/learner/allcourse">Explore Courses</Link></li>
+                    <li><Link href="/learner/mycourse">My Courses</Link></li>
                 </ul>
             </div>
             <div className="navbar-end">
