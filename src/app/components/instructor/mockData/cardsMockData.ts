61bede7ea062e8a148b5a90f3df61a43b7908a49
chamath-shanaka <chamathshanaka19@gmail.com>
2024-05-10 19:00:20 +0530

    feat: instructor dashboard

---
+++ src/app/components/instructor/mockData/cardsMockData.ts
@@ -0,0 +1,92 @@
+export const cardsMockData = [
+  {
+    courseTitle: "Course one",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Programming",
+    tags: ["TS", "Next", "MongoDB"],
+    price: 50,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course two",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Web Dev.",
+    tags: ["HTML", "CSS", "JavaScript"],
+    price: 60,
+    isApproved: false,
+  },
+  {
+    courseTitle: "Course three",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Data Science",
+    tags: ["Python", "Machine Learning", "Data Analysis"],
+    price: 70,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course four",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Graphic Design",
+    tags: ["Photoshop", "Illustrator", "UI/UX"],
+    price: 55,
+    isApproved: false,
+  },
+  {
+    courseTitle: "Course five",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Mobile Dev.",
+    tags: ["React Native", "Flutter", "Swift"],
+    price: 65,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course six",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Cloud Computing",
+    tags: ["AWS", "Azure", "Google Cloud"],
+    price: 80,
+    isApproved: false,
+  },
+  {
+    courseTitle: "Course seven",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Blockchain",
+    tags: ["Bitcoin", "Ethereum", "Smart Contracts"],
+    price: 75,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course eight",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "Cybersecurity",
+    tags: ["Ethical Hacking", "Network Security", "Penetration Testing"],
+    price: 90,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course nine",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "AI",
+    tags: ["Neural Networks", "Deep Learning", "Natural Language Processing"],
+    price: 100,
+    isApproved: true,
+  },
+  {
+    courseTitle: "Course ten",
+    imgUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+    category: "UI/UX",
+    tags: ["Adobe XD", "Figma", "Sketch"],
+    price: 70,
+    isApproved: true,
+  },
+];
--- src/app/instructor/page.tsx
+++ src/app/instructor/page.tsx
@@ -1,8 +1,9 @@
+import CourseList from "../components/instructor/CourseList";
 import Stats from "../components/instructor/Stats";
 
 export default function Page() {
   return (
-    <div className="p-5 flex flex-col items-center">
+    <div className=" max-w-[100vw] py-5 px-16 flex flex-col items-center">
       <h1 className="justify-center text-center">instructor dashboard</h1>
 
       {/* stats */}
@@ -11,6 +12,14 @@ export default function Page() {
       <button className="btn btn-info w-max my-8">+ New Course</button>
 
       {/* approved courses */}
+      <div className="min-w-full mt-40">
+        <CourseList title={'Approved courses'} isApproved={true} />
+      </div>
+
+      {/* pending courses */}
+      <div className="min-w-full mt-40">
+        <CourseList title={'Pending courses'} isApproved={false} />
+      </div>
     </div>
   )
 }
